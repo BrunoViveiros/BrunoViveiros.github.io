2a0f141568cd12678d58d962309009f50bb1ae8b
Bruno Fernandes <bruferviveiros@gmail.com>
2019-06-27 12:37:28 -0300

    mudança cor icones navbar

--- script.js
+++ script.js
@@ -16,17 +16,17 @@ window.onscroll = function () {
 
 	//Muda cor do icone se estiver na section correspondente
 	if ( scroll <= anch1 ) {
-		document.getElementById('nav-home').children[0].style.color = '#E08119';
+		document.getElementById('nav-home').children[0].style.color = '#FF851B';
     	document.getElementById('nav-projects').children[0].style.color = '';
 		document.getElementById('nav-contact').children[0].style.color = '';
     } else if ( scroll >= anch1 && scroll <= anch2 ) {
     	document.getElementById('nav-home').children[0].style.color = '';
-    	document.getElementById('nav-projects').children[0].style.color = '#00539C';
+    	document.getElementById('nav-projects').children[0].style.color = '#0074D9';
     	document.getElementById('nav-contact').children[0].style.color = '';
     } else {
     	document.getElementById('nav-home').children[0].style.color = '';
     	document.getElementById('nav-projects').children[0].style.color = '';
-    	document.getElementById('nav-contact').children[0].style.color = '#9B1B30';
+    	document.getElementById('nav-contact').children[0].style.color = '#01FF70';
 	}
 	
 
