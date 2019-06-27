91a7cb6626942efe5de4b900c71a86b93e7ce482
Bruno Fernandes <bruferviveiros@gmail.com>
2019-06-27 08:27:15 -0300

    identação e +comentarios

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ window.onscroll = function () {
     let anch2 = document.getElementById('contact-anchor').offsetTop;
     let scroll = window.innerHeight/2 + window.pageYOffset;
 	
+	//Faz navbar aparecer após section home e desaparecer antes
+	if ( scroll < anch1 ) {
+		document.getElementById('navbar').children[0].style.right = '-200px'
+		document.getElementById('navbar').children[1].style.right = '-200px'
+		document.getElementById('navbar').children[2].style.right = '-200px'
+	} else {
+		document.getElementById('navbar').children[0].style.right = '-70px'
+		document.getElementById('navbar').children[1].style.right = '-70px'
+		document.getElementById('navbar').children[2].style.right = '-70px'
+	}
+
+	//Muda cor do icone se estiver na section correspondente
 	if ( scroll <= anch1 ) {
 		document.getElementById('nav-home').children[0].style.color = '#E08119';
     	document.getElementById('nav-projects').children[0].style.color = '';
@@ -17,14 +29,6 @@ window.onscroll = function () {
     	document.getElementById('nav-contact').children[0].style.color = '#9B1B30';
 	}
 	
-	if ( scroll < anch1 ) {
-		document.getElementById('navbar').children[0].style.right = '-200px'
-		document.getElementById('navbar').children[1].style.right = '-200px'
-		document.getElementById('navbar').children[2].style.right = '-200px'
-	} else {
-		document.getElementById('navbar').children[0].style.right = '-70px'
-		document.getElementById('navbar').children[1].style.right = '-70px'
-		document.getElementById('navbar').children[2].style.right = '-70px'
-	}
+
 
 };
