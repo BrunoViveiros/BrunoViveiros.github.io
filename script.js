69a386865b5504f00f2c65e322423b1e369390d2
Bruno Fernandes <bruferviveiros@gmail.com>
2019-06-28 11:49:33 -0300

    Adicionado FOR para diminuir linhas de codigo

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 window.onscroll = function () { 
+	let items = document.getElementById('navbar').children.length
 	let anch1 = document.getElementById('skills-anchor').offsetTop;
 	let anch2 = document.getElementById('projects-anchor').offsetTop;
     let anch3 = document.getElementById('contact-anchor').offsetTop;
     let scroll = window.innerHeight/2 + window.pageYOffset;
 	
 	//Faz navbar aparecer após section home e desaparecer antes
-	if ( scroll < anch1 ) {
-		document.getElementById('navbar').children[0].style.right = '-200px'
-		document.getElementById('navbar').children[1].style.right = '-200px'
-		document.getElementById('navbar').children[2].style.right = '-200px'
-		document.getElementById('navbar').children[3].style.right = '-200px'
-	} else {
-		document.getElementById('navbar').children[0].style.right = '-70px'
-		document.getElementById('navbar').children[1].style.right = '-70px'
-		document.getElementById('navbar').children[2].style.right = '-70px'
-		document.getElementById('navbar').children[3].style.right = '-70px'
+	for (let i = 0; i < items; i++) {
+		if ( scroll < anch1 ) {
+			document.getElementById('navbar').children[i].style.right = '-200px'
+		} else {
+			document.getElementById('navbar').children[i].style.right = '-70px'
+		}
 	}
 
 	//Muda cor do icone se estiver na section correspondente
